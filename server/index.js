d14c298c6339cb8bdd03841386d9544f2efb3b65
t180miya <t.miya.c.180157@gmail.com>
2023-07-02 18:19:47 +0900

    test

--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const config = require('./config/dev')
+const config = require('./config')
 const SampleDb = require('./sample-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.set('strictQuery', true)
 mongoose.connect(config.DB_URI).then(
-  () => {
-    const sampleDb = new SampleDb()
-    sampleDb.initDb()
+// mongoose.connect(process.env.DB_URI).then(
+    () => {
+    if (process.env.NODE_ENV !== 'production'){
+      const sampleDb = new SampleDb()
+      //sampleDb.initDb()
+    }
   }
 )
 
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
+
+if (process.env.NODE_ENV === 'production'){
+  const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || '3001'
 
