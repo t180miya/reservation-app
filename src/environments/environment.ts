8da30075067db013a4b2ab183f1bd89a413d161d
t180miya <t.miya.c.180157@gmail.com>
2023-07-02 12:07:56 +0900

    ci/cd test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -14,3 +17,20 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7rOWfk96VdxQt2NRakJg_bcTtilJk0mc",
+  authDomain: "reservation-app-4d7e0.firebaseapp.com",
+  projectId: "reservation-app-4d7e0",
+  storageBucket: "reservation-app-4d7e0.appspot.com",
+  messagingSenderId: "793284151300",
+  appId: "1:793284151300:web:111a5b4c4fdf5a06254d51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
